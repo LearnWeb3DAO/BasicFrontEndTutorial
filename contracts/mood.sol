40f6e9ff541a477dc1e649ee9ff34d9df9d53106
AkashPattanaik <akashpattnaik.github@gmail.com>
2022-06-25 13:31:31 +0530

    Add SPDX-License and remove `memory` keyword

--- contracts/mood.sol
+++ contracts/mood.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 //specify the version of solidity
 pragma solidity ^0.8.1;
 
@@ -10,12 +12,12 @@ contract MoodDiary{
     string mood;
     
     //create a function that writes a mood to the smart contract
-    function setMood(string memory _mood) public{
+    function setMood(string _mood) public{
         mood = _mood;
     }
     
     //create a function the reads the mood from the smart contract
-    function getMood() public view returns(string memory){
+    function getMood() public view returns(string){
         return mood;
     }
 }
